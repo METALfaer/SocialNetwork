775f8314fb5efe09397d2548cded72e00113b15a
METALfaer <metalfaer4@gmail.com>
2023-05-24 18:00:49 +0300

    comit10

---
+++ src/redux/profile-reducer.test.ts
@@ -0,0 +1,27 @@
+import profileReducer, {addPostActionCreator, StateProfilePageType} from "./profile-reducer";
+
+let state: StateProfilePageType = {
+    posts: [
+        {id: 1, message: 'more many', likesCount: 14},
+        {id: 2, message: 'want meny many', likesCount: 32}
+    ],
+    profile: null,
+    status: ''
+}
+test('message of  post should be correct', () => {
+    let action = addPostActionCreator('it-camasutra')
+
+    let newState = profileReducer(state, action)
+
+    expect(newState.posts.length).toBe(3)
+})
+
+test('new post should be added', () => {
+
+    let action = addPostActionCreator('it-camasutra')
+
+    let newState = profileReducer(state, action)
+
+    expect(newState.posts[2].message).toBe('it-camasutra')
+})
+
