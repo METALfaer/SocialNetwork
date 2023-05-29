2137c47bd3ff6f1138e9b819a5ffa3f3b1e2532c
METALfaer <metalfaer4@gmail.com>
2023-05-29 21:15:48 +0300

    comit18

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,8 +1,9 @@
 import logo from "../../../logo.svg";
-import React, {ChangeEvent} from "react";
+import React, {ChangeEvent, useState} from "react";
 import s from './ProfileInfo.module.css'
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 import userPhoto from './../../assets/seriousSem.jpg'
+import {ProfileDataForm} from "./ProfileDataForm";
 
 
 type ProfileInfoType = {
@@ -14,15 +15,17 @@ type ProfileInfoType = {
 }
 export const ProfileInfo = (props: ProfileInfoType) => {
 
+    const [editMode, setEditMode] = useState(false)
+
     if (!props.profile) {
         return <div className='face'>
             <img src={logo} className={s.App_logo} alt="logo"/>
         </div>
     }
 
-    const onMainPhotoSelected=(e:ChangeEvent<HTMLInputElement>)=>{
+    const onMainPhotoSelected = (e: ChangeEvent<HTMLInputElement>) => {
         // @ts-ignore
-        if(e.target.files.length){
+        if (e.target.files.length) {
             // @ts-ignore
             props.savePhoto(e.target.files[0])
         }
@@ -36,9 +39,58 @@ export const ProfileInfo = (props: ProfileInfoType) => {
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large || userPhoto}/>
                 {props.isOwner && <input type={'file'} onChange={onMainPhotoSelected}/>}
+
+                {editMode
+                    ? <ProfileDataForm profile={props.profile}/>
+                    : <ProfileData profile={props.profile}
+                                   isOwner={props.isOwner}
+                                   goToEditMode={() => {
+                                       setEditMode(true)
+                                   }}
+                    />}
+
+
                 <ProfileStatusWithHooks status={props.status}
                                         updateStatus={props.updateUserStatus}/>
             </div>
         </div>
     )
+
+
+
+
+    const ProfileData = (profile: any, isOwner: any, goToEditMode: any) => {
+        return <div>
+            {props.isOwner && <div>
+                <button onClick={props.goToEditMode}>edit</button>
+            </div>}
+            <div>
+                <b>Full name: </b> {props.profile.fullName}
+            </div>
+            <div>
+                <b>Looking for a job: </b> {props.profile.lookingForAJob ? 'yes' : 'no'}
+            </div>
+            {props.profile.lookingForAJob &&
+            <div>
+                <b>My pfessional skils: </b> {props.profile.lookingForAJobDescription}
+            </div>
+            }
+            <div>
+                <b>About me: </b> {props.profile.aboutMe}
+            </div>
+            <div>
+                <b>Contacts: </b> {Object.keys(props.profile.contacts).map(key => {
+                return <Contact key={key} contactTitle={key} contactValue={props.contacts[key]}/>
+            })}
+            </div>
+        </div>
+    }
+
+    const Contact = ({contactTitle: any, contactValue: any}) => {
+        return (
+            <div className={s.contact}>
+                <b>{contactTitle}: </b>{contactValue}
+            </div>
+        )
+    }
 }
